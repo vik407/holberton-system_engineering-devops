74810e1c081770de354ddbc53442b79350b7be1e
Victor Hernandez <vik407@gmail.com>
2020-03-23 21:04:55 +0000

    Fix puppet identation

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # Using Puppet, create a manifest that kills a process named killmenow.
 
 exec { 'killmenow':
-  command  =>  'pkill -f ./killmenow',
-  path  =>  '/usr/bin/',
+  path    => '/usr/bin/',
+  command => 'pkill -f ./killmenow',
 }
