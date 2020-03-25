8975cc9bd56e5925e5cbd731a520379d2f6a23e2
Victor Hernandez <vik407@gmail.com>
2020-03-25 04:13:08 +0000

    Puppet advanced and 404 error template

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,1 +1,18 @@
-#!/usr/bin/env bash
+# 6. Install Nginx web server (w/ Puppet)
+
+package { 'nginx':
+  ensure => installed,
+  require => Package['nginx'],
+}
+
+file { '/usr/share/nginx/html/index.html':
+  content => 'Holberton School',
+  path    => '/usr/share/nginx/html/index.html'
+}
+
+file_line { 'redirect':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'server_name _;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;'
+}
