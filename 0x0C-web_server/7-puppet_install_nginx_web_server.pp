e6ac9e51dd3f458699654d398be70120d0e3bb73
Victor Hernandez <vik407@gmail.com>
2020-03-25 18:51:54 +0000

    Fix puppet advanced script

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,17 +2,23 @@
 
 package { 'nginx':
   ensure => installed,
-  require => Package['nginx'],
+  name   => 'nginx',
 }
 
-file { '/usr/share/nginx/html/index.html':
+file { '/var/www/html/index.html':
   content => 'Holberton School',
-  path    => '/usr/share/nginx/html/index.html'
+  path    => '/var/www/html/index.html'
 }
 
-file_line { 'redirect':
-  ensure => present,
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'server_name _;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;'
+file_line { 'title':
+  ensure   => present,
+  path     => '/etc/nginx/sites-available/default',
+  after    => 'server_name _;',
+  line     => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  multiple => true
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
 }
