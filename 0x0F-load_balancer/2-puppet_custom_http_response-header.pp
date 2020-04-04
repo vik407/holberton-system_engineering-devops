3999f6de5669041b93e8bebba526bfc20f4b43dc
Victor Hernandez <vik407@gmail.com>
2020-04-04 02:42:12 +0000

    puppet task

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,1 +1,32 @@
-#!/usr/bin/env bash
+# create a custom HTTP header response
+
+package { 'nginx':
+  ensure  => installed,
+  require => Exec['apt-get update']
+}
+
+file_line { 'redirect':
+    ensure  => 'present',
+    path    => '/etc/nginx/sites-available/default',
+    after   => 'server_name _;',
+    line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+    require => Package['nginx'],
+}
+
+file_line { 'header':
+    ensure  => 'present',
+    path    => '/etc/nginx/sites-available/default',
+    after   => 'server_name _;',
+    line    => 'add_header X-Served-By "$HOSTNAME";',
+    require => Package['nginx'],
+}
+
+file { '/var/www/html/index.html':
+  content => 'Holberton School',
+  require => Package['nginx'],
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
