0c539f9cb5782333fa474d91b749ea95c19fd921
Victor Hernandez <vik407@gmail.com>
2020-05-04 20:37:06 +0000

    1. Export to CSV 0x0

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,1 +1,35 @@
 #!/usr/bin/python3
+"""Python script that uses a REST API
+"""
+import requests
+from sys import argv
+import csv
+
+
+def get_usr_tocsv(uid):
+    """GET user todos at given id"""
+    url = "https://jsonplaceholder.typicode.com/"
+    endpoint_a = "users/"
+    endpoint_b = "todos?userId="
+    """Employee Name"""
+    query_employee = url + endpoint_a + uid
+    employee = requests.get(query_employee).json()
+    """Employee Tasks"""
+    query_tasks = url + endpoint_b + uid
+    tasks = requests.get(query_tasks).json()
+    """Concatenate two responses"""
+    data = {"employee": employee, "tasks": tasks}
+    """Export to csv"""
+    with open("{}.csv".format(uid), mode="w") as res:
+        out = csv.writer(res, quoting=csv.QUOTE_ALL)
+        employee_id = data.get("employee").get("id")
+        employee_usr = data.get("employee").get("username")
+        for o in data.get("tasks"):
+            out.writerow([
+                employee_id, employee_usr, o.get("completed"), o.get("title")
+            ])
+
+
+if __name__ == '__main__':
+    """Excecute it"""
+    get_usr_tocsv(argv[1])
