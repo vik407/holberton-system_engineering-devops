d5ef4ad92a0aae9a128b72a940ffd65c387573e8
Victor Hernandez <vik407@gmail.com>
2020-05-07 20:57:35 +0000

    Task 0 subreddit subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""A file to make a query to an endpoint
+"""
+from requests import request
+
+
+def number_of_subscribers(subreddit):
+    """A function that queries the Reddit API and returns the number
+    of subscribers"""
+    url = "https://api.reddit.com/r/{}/about".format(subreddit)
+    headers = {"User-Agent": "Python3"}
+    response = request("GET", url, headers=headers).json()
+    try:
+        subscribers = response['data']['subscribers']
+        return subscribers
+    except Exception:
+        return 0
