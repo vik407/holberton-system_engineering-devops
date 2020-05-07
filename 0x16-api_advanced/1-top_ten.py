9735bf4b071142e3d4ed77b47cad48b13ebb6f49
Victor Hernandez <vik407@gmail.com>
2020-05-07 21:30:34 +0000

    Get hot post in subreddit 0x0

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,1 +1,18 @@
 #!/usr/bin/python3
+"""A function that queries the Reddit API and prints the titles of the
+first 10 hot posts
+"""
+from requests import request
+
+
+def top_ten(subreddit):
+    """A function that queries the Reddit API and returns the number
+    of subscribers"""
+    url = "https://api.reddit.com/r/{}/hot/.json?limit=10".format(subreddit)
+    headers = {"User-Agent": "Python3"}
+    response = request("GET", url, headers=headers).json()
+    try:
+        for i in range(10):
+            print(response['data']['children'][i]['data']['title'])
+    except Exception:
+        return 0
