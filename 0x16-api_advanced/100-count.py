a92099b21f0385fb03dd4b2950cee4c8589317f2
Victor Hernandez <vik407@gmail.com>
2020-05-08 04:48:31 +0000

    Advanced one 0x10

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -17,24 +17,22 @@ def count_words(subreddit, word_list, after="", counter={}, ini=0):
     url = "https://api.reddit.com/r/{}/hot?after={}".format(subreddit, after)
     headers = {"User-Agent": "Python3"}
     response = request("GET", url, headers=headers).json()
+
     try:
+        top = response['data']['children']
+        _after = response['data']['after']
+        for item in top:
+            for word in counter:
+                counter[word] += item['data']['title'].lower(
+                    ).split(' ').count(word.lower())
+        if _after is not None:
+            count_words(subreddit, word_list, _after, counter, 1)
+        else:
+            str = sorted(counter.items(), key=lambda kv: kv[1], reverse=True)
+            for name, num in str:
+                if num != 0:
+                    print('{}: {}'.format(name, num))
+    except Exception:
         if response['error'] == 404:
             print('')
             return
-    except Exception:
-        try:
-            top = response['data']['children']
-            _after = response['data']['after']
-            for item in top:
-                for word in counter:
-                    counter[word] += item['data']['title'].lower(
-                        ).split(' ').count(word.lower())
-            if _after is not None:
-                count_words(subreddit, word_list, _after, counter, 1)
-            else:
-                str = sorted(counter.items(), key=lambda kv: kv[1], reverse=True)
-                for name, num in str:
-                    if num != 0:
-                        print('{}: {}'.format(name, num))
-        except Exception:
-            return None
