4a07741dca34deb2974516525e3c0be81862a958
Victor Hernandez <vik407@gmail.com>
2020-05-28 18:20:01 +0000

    typo error

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,11 @@
 # 0. Sky is the limit, let's bring that limit higher
 
 exec { 'raise-limits':
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
-  command => 'sed -i "s/-n 15/ -n 30000/g" /etc/default/nginx'
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+  command => 'sed -i "s/15/4096/g" /etc/default/nginx'
 }
 
 exec { 'restart-nginx':
-  path    => '/etc/init.d/',
-  command => 'nginx restart'
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
