d2e49ee8141a9ffcdce806053b8b63be25f00bd7
Victor Hernandez <vik407@gmail.com>
2020-05-28 18:08:53 +0000

    Back to basic raise more

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,7 +2,7 @@
 
 exec { 'raise-limits':
   path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
-  command => 'sed -ri "s/(ULIMIT=\"-n) [0-9]+/\1 10000/" /etc/default/nginx'
+  command => 'sed -i "s/-n 15/ -n 30000/g" /etc/default/nginx'
 }
 
 service { 'nginx':
