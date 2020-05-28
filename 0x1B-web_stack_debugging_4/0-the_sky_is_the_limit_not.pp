ee9b531328229e4a99ff9f2b555bbf26423b8a45
Victor Hernandez <vik407@gmail.com>
2020-05-28 18:54:08 +0000

    Working version

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,14 @@
 # 0. Sky is the limit, let's bring that limit higher
 
-exec { 'raise-limits':
-  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-  command => 'sed -i "s/15/4096/g" /etc/default/nginx'
+service { 'nginx':
+  ensure => 'running',
+  enable => true,
 }
 
-exec { 'restart-nginx':
-  command => 'nginx restart',
-  path    => '/etc/init.d/'
+file { '/etc/default/nginx':
+  ensure => present,
+} -> exec { 'ULIMIT':
+  notify  => Service['nginx'],
+  path    => '/bin/',
+  command => "sed -i 's/15/4096/g' /etc/default/nginx"
 }
